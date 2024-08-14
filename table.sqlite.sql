6d3dc4e9b3225abc67c4880a300f11f46b1f4cca
Andros924 <sir_vivi@hotmail.it>
2024-08-14 17:31:39 +0200

    esercizio table

---
+++ table.sqlite.sql
@@ -0,0 +1,19 @@
+-- TABLE
+CREATE TABLE books (
+  book_id integer PRIMARY key,
+  title TEXT NOT NULL,
+  genre TEXT NOT NULL,
+  publisher_yeah integer, 
+  isbn integer,
+  price integer,
+  rating integer,
+  stock_count integer
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
