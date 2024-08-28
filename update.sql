bd4d2be29166da9234060554b7262c088723ed8c
Andros924 <sir_vivi@hotmail.it>
2024-08-28 15:54:40 +0200

    correzione esercizi

--- update.sql
+++ update.sql
@@ -1,19 +1,4 @@
--- TABLE
-CREATE TABLE books (
-  book_id integer PRIMARY key,
-  title TEXT NOT NULL,
-  genre TEXT NOT NULL,
-  publisher_yeah integer, 
-  isbn integer,
-  price integer,
-  rating integer,
-  stock_count integer
-  , pubblisher text, publisher TEXT, pages_number INTEGER);
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
- 
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
+UPDATE Books
+SET price = 12.99,   
+    stock_count = stock_count - 1  
+WHERE title = 'War and Peace';
